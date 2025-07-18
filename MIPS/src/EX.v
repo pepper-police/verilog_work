630b6000cf630841012700979897bbf7686e123a
Matsumoto <295148f@gs.kochi-tech.ac.jp>
2025-07-18 14:20:25 +0900

    fix bug

--- MIPS/src/EX.v
+++ MIPS/src/EX.v
@@ -4,61 +4,61 @@ input CLK, RST;
 input [31:0] Ins, Rdata1, Rdata2, Ed32, nextPC;
 output [31:0] Result, newPC;
 
-wire [31:0] MUX2, MUX4, MUX5;
+wire [31:0] MUX2, MUX4, MUX5, alu_res;
 
-assign MUX2 = Ins[31:26] == R_FORM ? MUX2 : Ed32;
-assign MUX4 = Result == 0 ? nextPC : (nextPC + (Ed32 << 2));
+assign MUX2 = Ins[31:26] == R_FORM ? Rdata2 : Ed32;
+assign MUX4 = alu_res == 0 ? nextPC : (nextPC + (Ed32 << 2));
 assign MUX5 = (Ins[31:26] == JALR || Ins[5:0] == JR) ? Rdata1 : // 3
               (Ins[31:26] == J || Ins[5:0] == JAL) ? {nextPC[31:28], (Ins[25:0] << 2)} : // 2
               (Ins[31:26] == BEQ || Ins[31:26] == BNE) ? MUX4 : // 1
                nextPC; // 0
 
-assign Result = ALU(Ins, Rdata1, MUX2)
+assign alu_res = ALU(Ins, Rdata1, MUX2);
+assign Result = alu_res;
+assign newPC = MUX5;
 
-function [31:0] ALU(Ins, Rdata1, MUX2);
-input [31:0] Ins, Rdata1, MUX2;
-output [31:0] Result;
-begin
-    case(Ins[31:26])
-        R_FORM:
-        case(Ins[5:0])
-            ADD:
-                ALU = Rdata1 + MUX2;
-            ADDU:
-                ALU = $unsigned(Rdata1) + $unsigned(MUX2);
-            SUB:
-                ALU = Rdata1 - MUX2;
-            SUBU:
-                ALU = $unsigned(Rdata1) - $unsigned(MUX2);
-            AND:
-                ALU = Rdata1 & MUX2;
-            OR:
-                ALU = Rdata1 | MUX2;
-            XOR:
-                ALU = Rdata1 ^ MUX2;
-            NOR:
-                ALU = Rdata1 ~| MUX2;
-            SLT:
-                ALU = Rdata1 < MUX2;
-            SLTU:
-                ALU = $unsigned(Rdata1) < $unsigned(MUX2);
-        endcase
-        ADDI:
-            ALU = Rdata1 + MUX2;
-        ADDIU:
-            ALU = $unsigned(Rdata1) + $unsigned(MUX2);
-        SLTI:
-            ALU = Rdata1 < MUX2;
-        ANDI:
-            ALU = Rdata1 & MUX2;
-        ORI:
-            ALU = Rdata1 | MUX2;
-        XORI:
-            ALU = Rdata1 ^ MUX2;
+function [31:0] ALU(input[31:0] f_Ins, f_Rdata1, f_MUX2);
+case(f_Ins[31:26])
+    R_FORM:
+    case(f_Ins[5:0])
+        ADD:
+            ALU = f_Rdata1 + f_MUX2;
+        ADDU:
+            ALU = $unsigned(f_Rdata1) + $unsigned(f_MUX2);
+        SUB:
+            ALU = f_Rdata1 - f_MUX2;
+        SUBU:
+            ALU = $unsigned(f_Rdata1) - $unsigned(f_MUX2);
+        AND:
+            ALU = f_Rdata1 & f_MUX2;
+        OR:
+            ALU = f_Rdata1 | f_MUX2;
+        XOR:
+            ALU = f_Rdata1 ^ f_MUX2;
+        NOR:
+            ALU = ~(f_Rdata1 | f_MUX2);
+        SLT:
+            ALU = f_Rdata1 < f_MUX2;
+        SLTU:
+            ALU = $unsigned(f_Rdata1) < $unsigned(f_MUX2);
         default:
-            ALU = 0
+            ALU = 32'hxxxxxxxx;
     endcase
-end
+    ADDI:
+        ALU = f_Rdata1 + f_MUX2;
+    ADDIU:
+        ALU = $unsigned(f_Rdata1) + $unsigned(f_MUX2);
+    SLTI:
+        ALU = f_Rdata1 < f_MUX2;
+    ANDI:
+        ALU = f_Rdata1 & f_MUX2;
+    ORI:
+        ALU = f_Rdata1 | f_MUX2;
+    XORI:
+        ALU = f_Rdata1 ^ f_MUX2;
+    default:
+        ALU = 32'hxxxxxxxx;
+endcase
 endfunction
 
 
