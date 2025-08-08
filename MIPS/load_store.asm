e2d138c1603702ac7bd9dc83d00ef10570fbe5c8
okayu0518 <250376s@ugs.kochi-tech.ac.jp>
2025-08-08 10:01:56 +0900

    add load-store test

---
+++ MIPS/load_store.asm
@@ -0,0 +1,16 @@
+.set noreorder
+
+.global _start
+_start:
+	addi $7, 0
+	lw	$s0, 0($s7)
+	lw	$s1, 4($s7)
+	addi $t0, $s7, 8
+	sw $s1, 0($t0)
+loop:
+	j loop
+	
+.data
+a: .word 10
+b: .word 20
+array1: .space 16
